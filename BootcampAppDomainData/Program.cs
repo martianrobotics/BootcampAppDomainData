17ece75e9cc26ae61b2fe346813e99322e83222f
martianrobotics <jupiter.arias@gmail.com>
2017-07-05 15:59:08 -0400

    Add project files.

---
+++ BootcampAppDomainData/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace BootcampAppDomainData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fetching current domanin");
+            // use current appDomain, and store some data
+            AppDomain domain = System.AppDomain.CurrentDomain;
+            Console.WriteLine("Setting appDomain data");
+            string name = "MyData";
+            string value = "Some data to store";
+            domain.SetData(name, value);
+            Console.WriteLine("Fetching Domain Data");
+            Console.WriteLine("The data found for key {0} is {1}",
+                name, domain.GetData(name));
+            Console.ReadLine();
+        }
+    }
+}
